ccdbe553e484625fe1c2aa10264f4297d104554a
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-29 20:32:57 +0000

    Cleaned code up a little

--- iss.js
+++ iss.js
@@ -4,7 +4,7 @@ const request = require('request');
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error, response, body) =>{
     if (error) {
-      callback(error);
+      callback(error, null);
       return;
     }
     if (response.statusCode !== 200) {
@@ -12,7 +12,8 @@ const fetchMyIP = function(callback) {
       callback(Error(msg), null);
       return;
     }
-    callback(null, JSON.parse(body).ip);
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
     return;
   });
 };
